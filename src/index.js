e8168cc26cf3fd461b4719bfa3a4bec31a63f983
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-21 21:58:42 -0700

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './pages/App/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route render={(props) => 
+        <App {...props} />
+      }
+    />
+  </Router>,
   document.getElementById('root')
 );
