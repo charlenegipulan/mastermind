be2dcc4772d0e9c9871b8e8f3af2cef541b3f231
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-08-21 21:39:44 -0700

    add and connect database

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 
 var app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api', require('./routes/api'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
